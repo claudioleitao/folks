b13bd5f7f0c320378534fdcbca803c58433deda7
Cláudio Fernando Leitão <claudio@pop-os.localdomain>
2022-09-06 16:20:48 -0300

    Início do projeto

---
+++ backend/src/main/java/com/blacksungamer/Folks/FolksApplication.java
@@ -0,0 +1,13 @@
+package com.blacksungamer.Folks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/blacksungamer/Folks/FolksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blacksungamer.Folks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
